b7d272e2604b8a0d5392f5117a5269e92339f746
Vika Miderova <Slarkmidd@mail.ru>
2025-10-21 18:36:40 +0300

    Начало

---
+++ script.js
@@ -0,0 +1,12 @@
+class TodoApp{
+    constructor() {
+        this.todoList = [];
+
+        this.todoInput = document.querySelector
+        ("[data-add-todo-input]");
+        this.todoContainer = document.querySelector
+        ("[data-todos-container]");
+    }
+}
+
+new TodoApp();
